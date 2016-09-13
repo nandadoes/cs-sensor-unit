caf314bbf6fc083eb1642aef6fc91bd885841fbe
nandadoes <nandadoes@gmail.com>
2016-09-13 14:48:31 +0100

    Update

--- data/export_json.R
+++ data/export_json.R
@@ -1,48 +1,28 @@
-# http://www.r-tutor.com/r-introduction/data-frame/data-import
-# dfrm <- read.table("someTxtFiles/03072819.TXT", sep=",", col.names=c("id", "ligh", "airTemp", "airHum", "soilTemp", "soilHum", "airPress", "remainV"))
-# dfrm <- read.table("someTxtFiles/03072819.TXT")
-# print(dfrm)
+# export-json.R
+# 
+# This scirpt import content of a csv file into a dataframe then convert it into
+# a json object before saving it in JSON file format
+# 
+# Requuired package:
+#         jsonlite
+# 
+# Last modified: 2016-09-12  
+# By: Nanda Khaorapapong
+# Contact: nandadoes@gmail.com  
+#    
+# Copyright (c) 2016 Nanda Khaorapapong and Connected Seeds  
+# License: GNU GPLv3 license <https://www.gnu.org/licenses/gpl-3.0.en.html>
+#         
+        
+d <- read.table("SEEDS01/batch.csv", sep=",", 
+                col.names=c("id", "light", "airTemp", "airHum", "soilTemp", 
+                            "soilHum", "airPress", "remainV"))
 
-# dfrm1 <- read.table("someTxtFiles/03072820.TXT", sep=",", col.names=c("id", "light", "airTemp", "airHum", "soilTemp", "soilHum", "airPress", "remainV"))
-# print(dfrm1)
+json_d <- toJSON(d, pretty=TRUE)
 
+cat(json_d)
 
-# How to 
-# 1) merge all .txt files in a folder into a .cvs file via terminal u
-# using the following command:
-#         cat *.TXT >merged.csv
-# REF: merge multiple file with mac os x terminal http://monchito.com/blog/merge-csv-files-with-mac-os-x-terminal
+write(json_d, "seeds01.json")
 
-# 2) import the merged file into a dataframe using read.table with colnames parameters
-d <- read.table("SEEDS01_CVS/_merged.csv", sep=",", col.names=c("id", "light", "airTemp", "airHum", "soilTemp", "soilHum", "airPress", "remainV"))
-# d <- read.table("someTxtFiles_2/merged.csv", sep=",", col.names=c("id", "light", "airTemp", "airHum", "soilTemp", "soilHum", "airPress", "remainV"))
-# print(d)
-
-# 3) export to JSON using package: jsonlite
-# https://cran.r-project.org/web/packages/jsonlite/vignettes/json-aaquickstart.html 
-# https://www.r-bloggers.com/new-package-jsonlite-a-smarter-json-encoderdecoder/
-
-# install 
-install.packages("jsonlite", repos="http://cran.r-project.org")
-
-# load 
-library(jsonlite)
-
-# Convert dataframe object into json object 
-json_data <- toJSON(d, pretty=TRUE)
-# Print output to the screen using cat() function see: http://rfunction.com/archives/1866
-# cat(json_data)
-## Export to JSON format using jsonlite library
-write(json_data, "Export_files/seeds01.json")
-
-## Revert json object to dataframe object
-# df_data <- fromJSON(json_data)
-# # Export the dataframe object into a text file
-# write.table(d, "Export_files/testWriteD.txt", sep="\t")
-
-## ...into Excel spreadsheet 
-# install.packages("xlsx", repos="http://cran.r-project.org")
-# library(xlsx)
-# write.xlsx(d, "Export_files/testWriteD.xlsx")
 
 
